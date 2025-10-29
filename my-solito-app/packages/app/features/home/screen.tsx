4664bc9eadb7b4603f872a805532f1a672a39e75
romanlesnikov <roman.lesnikov@gmail.com>
2025-10-29 16:52:07 +0200

    add libs

--- my-solito-app/packages/app/features/home/screen.tsx
+++ my-solito-app/packages/app/features/home/screen.tsx
@@ -2,8 +2,33 @@
 
 import { TextLink } from 'solito/link'
 import { Text, View } from 'react-native'
-
+import { useEffect } from 'react'
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, withSpring, withSequence, Easing } from 'react-native-reanimated'
+import { VirtuosoGrid, Virtuoso } from 'react-virtuoso'
+import { Panel , PanelGroup , PanelResizeHandle } from "react-resizable-panels";
+import { Platform } from 'react-native'
 export function HomeScreen() {
+const isWeb = Platform.OS == 'web'
+    const opacity = useSharedValue(0);
+  const translateY = useSharedValue(-50); // start position
+  const animationDuration = 500;
+  useEffect(() => {
+    // Start the animations
+    opacity.value = withTiming(1, { duration: animationDuration });
+    translateY.value = withSpring(0);
+}, []); // <-- Empty dependency array ensures this runs only once on mount
+
+const animatedStyles = useAnimatedStyle(() => {
+  return {
+      opacity: opacity.value,
+      transform: [
+          {
+              translateY: translateY.value
+          }
+      ]
+  };
+},[opacity, translateY]);
+
   return (
     <View
       style={{
@@ -14,7 +39,23 @@ export function HomeScreen() {
         gap: 32,
       }}
     >
-      <H1>Welcome to Solito.</H1>
+      <Animated.View style={[animatedStyles]}>
+        <H1>Welcome to Solito.</H1>
+      </Animated.View>
+      {isWeb && <Virtuoso></Virtuoso>}
+       {isWeb && <PanelGroup direction="horizontal">
+   <Panel defaultSize={30} minSize={20} style={{background:'red'}}>
+     left
+   </Panel>
+   <PanelResizeHandle />
+   <Panel minSize={30}>
+     middle
+   </Panel>
+   <PanelResizeHandle />
+   <Panel defaultSize={30} minSize={20}>
+     right
+   </Panel>
+ </PanelGroup>}
       <View style={{ maxWidth: 600, gap: 16 }}>
         <Text style={{ textAlign: 'center' }}>
           Here is a basic starter to show you how you can navigate from one
